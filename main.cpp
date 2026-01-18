5fd19069cc31b32cd4fbd8dbeae3632f0e0326c6
sofie-m <124958662+sofie-m@users.noreply.github.com>
2026-01-17 17:39:22 -0700

    Create window with glfw

--- main.cpp
+++ main.cpp
@@ -7,6 +7,27 @@
 
 int main()
 {
+	// Instantiate GLFW window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create window object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "window!", NULL, NULL);
+	if (window == NULL)
+	{
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLAD
+	if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+		return -1;
+	}
+
+
 	//PhysX management class instances.
 	physx::PxDefaultAllocator gAllocator;
 	physx::PxDefaultErrorCallback gErrorCallback;
@@ -81,6 +102,16 @@ int main()
 
 	// Clean up
 	shape->release();
+
+	// Window test
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+	
 
 	// Simulate at 60fps
 	while (1)
