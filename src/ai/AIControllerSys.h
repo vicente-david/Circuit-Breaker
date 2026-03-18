0a03c1c57221eb3243a9e34560a13c2e6605bac1
sofie-m <124958662+sofie-m@users.noreply.github.com>
2026-03-18 15:39:18 -0600

    delete comments

--- src/ai/AIControllerSys.h
+++ src/ai/AIControllerSys.h
@@ -14,15 +14,9 @@ class AIControllerSys : public System {
 		static std::shared_ptr<AIControllerSys> registerSystem(std::shared_ptr<Coordinator>& coord);
 
 		void AI_IDLE(AIController &ai, SparkControls &controls, GameState& game);
-		/*void AI_DRIVING(AIController& ai, SparkControls& controls, Transform& transform, SparkData& spark);
-		void AI_BRAKING(AIController& ai, SparkControls& controls, Transform& transform, SparkData& spark);
-		void AI_DRIFTING(AIController& ai, SparkControls& controls, Transform& transform, SparkData& spark);
-		void AI_BOOSTING(AIController& ai, SparkControls& controls, Transform& transform, SparkData& spark);
-		void AI_ATTACKING(AIController& ai, SparkControls& controls, Transform& transform);*/
+		
 
 private:
 	
 	float arrivalRadius = 14.0f; // how close a spark needs to be from the targetPos to consider it as "arrived"
-
-	//void calcSteering(AIController& ai, SparkControls& controls, Transform& transform, SparkData& spark);
 };
